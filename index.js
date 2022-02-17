8875ace25e0fef76603831dd1e7e3a43575c6ab5
Romy-thu <romythu.@emcreative.studio>
2022-02-17 14:56:56 +0100

    aanpassingen

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-console.log("Hallo mensen")
+// console.log("Hallo mensen")
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+express()
+app.get('/', onhome)
+app.get('/about', onabout)
+app.get('/login', onlogin)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+.listen(8000)
+
+function onhome(req, res) {
+  res.send('<h1>Home Page</h1>')
+}
+
+function onabout(req, res) {
+  res.send('<h1>about me</h1>')
+}
+
+function onlogin(req, res) {
+  res.send('<h1>login</h1>')
+}
+
+app.get((req, res, next) => {
+  res.status(404).send('jammerdan')
 })
