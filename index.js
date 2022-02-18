bf44fa2c76be6096ea004a6c91d83812e9fe819c
Romy-thu <romythu.@emcreative.studio>
2022-02-18 15:30:31 +0100

    index.js foto aanpassing

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
 const express = require('express')
 const app = express()
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('static'))
 app.set('view engine', 'hier mijn templating')
 app.set('views', 'view')
 
