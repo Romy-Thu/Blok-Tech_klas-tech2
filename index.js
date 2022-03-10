5dc46e205593f647d02e86698117c24c01fd9ec2
Romy-thu <romythu.@emcreative.studio>
2022-03-10 13:42:58 +0100

    Database gegevens invoeren

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const exphbs = require("express-handlebars");
+const bodyParser = require('body-parser')
+const multer = require('multer')
 const PORT = 3001
+
 const {test}  = require('./utils/db')
 
 
@@ -8,20 +12,16 @@ require('dotenv').config();
 
 // ----------------------------------- Mongodb
 const { MongoClient } = require('mongodb');
-
-// 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blok-tech.xaela.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 console.log(uri)
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 console.log(test)
 test(client)
 
 // ----------------------------------- render pages
 
-const exphbs = require("express-handlebars");
 app.engine(
   "hbs",
   exphbs.engine({
@@ -37,21 +37,59 @@ app.get("/", (req, res) => {
 });
 
 
+
+// alle dieren die in array staan in console weergeven
+test(client).then(data => { console.log(data)})
+
+
+//-------------------------------------------------------------------------------------- naar database
+
+// middleware om omtegaan met incoming data in de body van een request. In dit geval POST
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+// async geeft aan dat dit een funcite is waarin dingen langer duren, zoals data uit een database halen
+// res.render() wordt gebruikt om een view te renderen en verstuurd de gerenderde HTML naar de client. 
+// duurt wat langer nog even wachten...
+app.get("/", async(req, res) => {
+
+  // data uit de database wat in een array is gestopt wordt nu in de constante dieren gezet.
+  const gebruikers = await test(client); 
+
+  // ophalen gebruikers database
+  res.render("matches", {
+    
+    gebruikers: gebruikers
+  });
+});
+
+
+app.post("/formulier", async(req, res) => {
+
+  const gebruikers = await test(client); 
+
+  console.log(req.body);
+  // filter gebruikers
+  const filterGebruikers = gebruikers.filter((gebruikers) => {
+    // stop het item alleen in de array wanneer onderstaande regel 'true' is
+    return gebruikers.Intresse == req.body.Intresse;
+  });
+  //render same page with filtered animals
+  res.render("matches", {
+    gebruikers: filterGebruikers
+  });
+});
+
 // ----------------------------------- routes maken
-
-app.use('/static', express.static('static'))
-
-app.get('/', onhome)
 app.get('/about', onabout)
 app.get('/login', onlogin)
 app.get('*', notfound)
 
-function onhome(req, res) {
-  res.send('<h1>Dit is de homepagina!</h1>')
-}
 
 function onabout(req, res) {
-  res.send('<h1>About!</h1>')
+  res.send('<h1>Hier vind je alles about me!</h1>')
 }
 
 function onlogin(req, res) {
@@ -62,6 +100,7 @@ function notfound(req, res) {
   res.send('<h1>404 - Not Found!</h1>')
 }
 
+// Geeft aan dat de app draait op de poort 8000
 app.listen(PORT, function () {
   console.log('listening to port: ', PORT)
 })
--- utils/db.js
+++ utils/db.js
@@ -6,14 +6,19 @@ exports.test = async function (client){
   
       const collection = database.collection('users')
   
-      const doc = {
-          naam: 'romy'
-        }
+      return collection.find().toArray()
+      // const doc = {
+      //       naam: "Michael B Jordan",
+      //       imgSrc: "static/images/persoon2.png",
+      //       Leeftijd: "30",
+      //       locatie: "Amsterdam",
+      //       Intresse: "Music",
+      //   }
   
-      // hier gaat een document in - fuctie insertOne (promises)
-      await collection.insertOne(doc).then(event => {
-        console.log('event', event)
-      })
+      // // hier gaat een document in - fuctie insertOne (promises)
+      // await collection.insertOne(doc).then(event => {
+      //   console.log('event', event)
+      // })
   
     // Error, als de database niet doet
     } catch (err) { 
