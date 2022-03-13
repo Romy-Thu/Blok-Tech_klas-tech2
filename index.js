1315d6a39b3b4f1f702ca019c04de7c0c8e44992
Romy-thu <romythu.@emcreative.studio>
2022-03-13 10:06:51 +0100

    Code prettier+ Comment

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const exphbs = require("express-handlebars");
-const bodyParser = require('body-parser')
-const multer = require('multer')
-const PORT = process.env.PORT || 3000
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const PORT = process.env.PORT || 3000;
 
-const {test}  = require('./utils/db')
+const { test } = require("./utils/db");
 
-
-require('dotenv').config();
+require("dotenv").config();
 
 // ----------------------------------- Mongodb
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@blok-tech.xaela.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-test(client)
+test(client);
 
 // ----------------------------------- render pages
 app.engine(
@@ -27,100 +29,94 @@ app.engine(
 );
 
 app.set("view engine", "hbs");
-app.set('views', './Views');
+app.set("views", "./Views");
 
 // alle dieren die in array staan in console weergeven
-test(client).then(data => { console.log(data)})
+test(client).then((data) => {
+  console.log(data);
+});
 
 // om images en css te serven in directory "static"
-app.use('/static', express.static('static'));
+app.use("/static", express.static("static"));
 
 //-------------------------------------------------------------------------------------- naar database
 
 // middleware om omtegaan met incoming data in de body van een request. In dit geval POST
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // async geeft aan dat dit een funcite is waarin dingen langer duren, zoals data uit een database halen
-// res.render() wordt gebruikt om een view te renderen en verstuurd de gerenderde HTML naar de client. 
+// res.render() wordt gebruikt om een view te renderen en verstuurd de gerenderde HTML naar de client.
 // duurt wat langer nog even wachten...
-app.get("/", async(req, res) => {
-
+app.get("/", async (req, res) => {
   // data uit de database wat in een array is gestopt wordt nu in de constante gebruikers gezet.
-  const gebruikers = await test(client); 
+  const gebruikers = await test(client);
 
-  console.log(gebruikers)
-
+  console.log(gebruikers);
 
   // ophalen gebruikers database
   res.render("matches", {
-    gebruikers: gebruikers
+    gebruikers: gebruikers,
   });
 });
 
-
-app.post("/formulier", async(req, res) => {
-
-  const gebruikers = await test(client); 
+app.post("/formulier", async (req, res) => {
+  const gebruikers = await test(client);
   const inputIntresse = req.body.Intresse;
   const inputLeeftijd = req.body.Leeftijd;
-  
-  // console.log(inputIntresse);
+
   console.log(inputLeeftijd);
   let tempArray = [];
 
-  gebruikers.forEach(object => {
-    if(object.Intresse == inputIntresse && object.Leeftijd == inputLeeftijd) {
-    tempArray.push(object);
+  gebruikers.forEach((object) => {
+    if (object.Intresse == inputIntresse && object.Leeftijd == inputLeeftijd) {
+      tempArray.push(object);
     }
-
   });
 
-
   // delete uit de database
-
+  // werkt nog niet 
   app.post("/delete", async (req, res) => {
+    await client.connect(client);
 
-    await client.connect(client)
-  
-    console.log(req.body)
-    console.log(req.body.match)
-  
-    client.db('userdb').collection('users').deleteOne({ naam: req.body.match }).then(LoreHarvet => {
-      console.log(LoreHarvet);
-    })
-  
-    res.redirect('/')
-  
-  });  
+    console.log(req.body);
+    console.log(req.body.match);
+
+    client.db("userdb").collection("users").deleteOne({ naam: req.body.match }).then((LoreHarvet) => {
+        console.log(LoreHarvet);
+      });
+
+    res.redirect("/");
+  });
 
   //render same page with filter gebruikers
   res.render("matches", {
-    gebruikers: tempArray
+    gebruikers: tempArray,
   });
 });
 
 // ----------------------------------- routes maken
-app.get('/about', onabout)
-app.get('/login', onlogin)
-app.get('*', notfound)
-
+app.get("/about", onabout);
+app.get("/login", onlogin);
+app.get("*", notfound);
 
 function onabout(req, res) {
-  res.send('<h1>Hier vind je alles about me!</h1>')
+  res.send("<h1>Hier vind je alles about me!</h1>");
 }
 
 function onlogin(req, res) {
-  res.send('<h1>Op deze pagina kun je inloggen</h1>')
+  res.send("<h1>Op deze pagina kun je inloggen</h1>");
 }
 
 function notfound(req, res) {
-  res.send('<h1>404 - Not Found!</h1>')
+  res.send("<h1>404 - Not Found!</h1>");
 }
 
 // Geeft aan dat de app draait op de poort 3000
 app.listen(PORT, function () {
-  console.log('listening to port: ', PORT)
-})
+  console.log("listening to port: ", PORT);
+});
